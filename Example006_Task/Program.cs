8a5838676be8c322603797df2c0e0cc3da56156a
Stanislav <stas2001.nikitin@yandex.ru>
2022-11-17 20:48:51 +0400

    Example006_Task_гири_рандом

--- Example006_Task/Program.cs
+++ Example006_Task/Program.cs
@@ -1,9 +1,13 @@
-﻿int a = 1;
-int b = 8;
-int c = 3;
-int d = 2;
-int e = 6;
-
+﻿int a = new Random().Next(1, 10);
+Console.WriteLine(a);
+int b = new Random().Next(1, 10);
+Console.WriteLine(b);
+int c = new Random().Next(1, 10);
+Console.WriteLine(c);
+int d = new Random().Next(1, 10);
+Console.WriteLine(d);
+int e = new Random().Next(1, 10);
+Console.WriteLine(e);
 int max = a;
 
 if (a > max) max = a;
